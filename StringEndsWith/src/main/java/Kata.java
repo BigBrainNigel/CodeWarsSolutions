71d0643913f257983f6a1cccdbc708765553ce4b
Hayden Walker <78957275+BigBrainNigel@users.noreply.github.com>
2023-04-15 04:39:04 -0500

    Add files via upload

---
+++ StringEndsWith/src/main/java/Kata.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * Created by Hayden Walker
+ *      4/15/23
+ *
+ * I honestly have no idea why this is even a kata lol. Probably gonna
+ * try and make an overcomplicated version sometime in the future.
+ *
+ */
+public class Kata {
+
+    public static boolean solution(String str, String ending) {
+        return str.endsWith(ending);
+    }
+
+}
